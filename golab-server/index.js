910b8d5929b15439de388db1c4ec5f70cdd0f42d
FreaksOnE <samanasady@gmail.com>
2018-04-08 00:57:51 +0430

    update

--- golab-server/index.js
+++ golab-server/index.js
@@ -25,11 +25,13 @@ var cors = require("cors");
 var whitelist = ["http://localhost:8080", "http://192.168.1.10:3001", "http://192.168.1.10", "*", ];
 var corsOptions = {
 	origin: function (origin, callback) {
-		if (whitelist.indexOf(origin) !== -1) {
+		callback(null, true);
+		return;
+		/*if (whitelist.indexOf(origin) !== -1) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
-		}
+		}*/
 	},
 };
 
